12eddc6a4f59fc85703187d5c5cc614bb7189d6f
llllcccjjj <70000656+llllcccjjj@users.noreply.github.com>
2021-02-09 16:59:28 +0800

    Update crontab_list.sh

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -1,6 +1,8 @@
 # 京喜财富岛
-5 8,13,19 * * * node /scripts/jx_cfd.js >> /scripts/logs/jx_cfd.log 2>&1
+5 8,13,19 * * * node /scripts/jd_jxcfd.js >> /scripts/logs/jd_jxcfd.log 2>&1
 
 # 京喜财富岛tx
-0 0 * * * node /scripts/jx_cfdtx.js >> /scripts/logs/jx_cfdtx.log 2>&1
+0 0 * * * node /scripts/jd_jxcfdtx.js >> /scripts/logs/jd_jxcfdtx.log 2>&1
 
+#百变大咖秀
+10 10,11 * * 2-5 node  /scripts/jd_entertainment.js >>  /scripts/logs/jd_entertainment.log 2>&1
