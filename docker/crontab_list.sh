c558d9901ae69d6527295b48d126d9bdb0298282
llllcccjjj <70000656+llllcccjjj@users.noreply.github.com>
2021-02-12 22:04:04 +0800

    Update crontab_list.sh

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -7,9 +7,6 @@
 #百变大咖秀
 10 10,11 * * 2-5 node  /scripts/jd_entertainment.js >>  /scripts/logs/jd_entertainment.log 2>&1
 
-#年货红包
-0 0,20-23 10-12 2 * node /scripts/jd_cxhb.js >> /scripts/logs/jd_cxhb.log 2>&1
-
 #国际盲盒
 0 0,20-23 10-12 2 * node /scripts/jd_gjmh.js >> /scripts/logs/jd_gjmh.log 2>&1
 
