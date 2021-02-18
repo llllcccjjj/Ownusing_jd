baefe21810c4369a64a2a8b0f9c3c121750ee548
llllcccjjj <70000656+llllcccjjj@users.noreply.github.com>
2021-02-18 21:34:27 +0800

    Update crontab_list.sh

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -7,10 +7,8 @@
 #百变大咖秀
 10 10,11 * * 2-5 node  /scripts/jd_entertainment.js >>  /scripts/logs/jd_entertainment.log 2>&1
 
-#国际盲盒
-0 0,20-23 10-12 2 * node /scripts/jd_gjmh.js >> /scripts/logs/jd_gjmh.log 2>&1
+#沃尔玛
+3 10 17-28 2 * node /scripts/jd_walmart.js >> /scripts/logs/jd_walmart.log 2>&1
 
-#红包雨
-0,50 0,9,11,13,15,17,19,20,21,23 8-18 2 * node /scripts/jd_red_rain.js >> /scripts/logs/jd_red_rain.log 2>&1
 
 
