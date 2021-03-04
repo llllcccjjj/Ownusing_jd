409a85457b83c3ddf737d45b3b0efffd25abe30d
llllcccjjj <70000656+llllcccjjj@users.noreply.github.com>
2021-03-04 13:47:02 +0800

    Update jd_jxcfd.js

--- jd_jxcfd.js
+++ jd_jxcfd.js
@@ -93,7 +93,7 @@ const $ = new Env("京喜财富岛");
 const JD_API_HOST = "https://m.jingxi.com/";
 const notify = $.isNode() ? require('./sendNotify') : '';
 const jdCookieNode = $.isNode() ? require("./jdCookie.js") : "";
-const jdTokenNode = $.isNode() ? require('./jdJxncTokens.js') : '';
+const jdTokenNode = $.isNode() ? require('./jd_JxncTokens.js') : '';
 $.showLog = $.getdata("cfd_showLog") ? $.getdata("cfd_showLog") === "true" : false;
 $.notifyTime = $.getdata("cfd_notifyTime");
 $.result = [];
