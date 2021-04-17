0a76ad2de133486a14ff5ce1c1fd4b5770feea19
llllcccjjj <70000656+llllcccjjj@users.noreply.github.com>
2021-04-17 22:07:41 +0800

    Update jd_live_hour_redrain.js

--- jd_live_hour_redrain.js
+++ jd_live_hour_redrain.js
@@ -35,7 +35,7 @@ let ids = {
   '20': 'RRA4RhWMc159kA62qLbaEa88evE7owb',
   '21': 'RRA4RhWMc159kA62qLbaEa88evE7owb',
   '22': 'RRA3gsDuBBP5kYqknjTZ2h3VLkEpiT3',
-  '23': 'RRA4RhWMc159kA62qLbaEa88evE7owb'
+  '23': 'RRA2qAfXJe5iMPfa7f5VCgviBaxjG3D'
 }
 const notify = $.isNode() ? require('./sendNotify') : '';
 //Node.js用户请在jdCookie.js处填写京东ck;
