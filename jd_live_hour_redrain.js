de888f0498d3b8630a3ce20ccf9454c63f5a65c2
llllcccjjj <70000656+llllcccjjj@users.noreply.github.com>
2021-04-20 05:46:08 +0800

    Update jd_live_hour_redrain.js

--- jd_live_hour_redrain.js
+++ jd_live_hour_redrain.js
@@ -19,6 +19,8 @@ let bodyList = {
   }
 }
 let ids = {
+  '5': 'RRA4RhWMc159kA62qLbaEa88evE7owb',
+  '6': 'RRA4RhWMc159kA62qLbaEa88evE7owb',
   '7': 'RRA4RhWMc159kA62qLbaEa88evE7owb',
   '8': 'RRA4RhWMc159kA62qLbaEa88evE7owb',
   '9': 'RRA4RhWMc159kA62qLbaEa88evE7owb',
